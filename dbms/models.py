061c161f8ecc2da50400590c71f7a4cbd386e6ed
dante <dkhoche70@gmail.com>
2019-10-14 17:45:52 +0530

    Indented all the Files

--- dbms/models.py
+++ dbms/models.py
@@ -1,31 +1,31 @@
 from django.db import models
 
 # Create your models here
+
+
 class booking(models.Model):
-    
-    fname=models.CharField( max_length=50)
-    lname=models.CharField( max_length=50)
-    email=models.EmailField(max_length=254)
-    city=models.CharField(max_length=50)
-    state=models.CharField(max_length=50)
-    pin=models.IntegerField()
+
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pin = models.IntegerField()
+
 
 def __str__(self):
-        return self.fname
-    
+    return self.fname
+
+
 class approved(models.Model):
-    
-    fname=models.CharField( max_length=50)
-    lname=models.CharField( max_length=50)
-    email=models.EmailField(max_length=254)
-    city=models.CharField(max_length=50)
-    state=models.CharField(max_length=50)
-    pin=models.IntegerField()
+
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pin = models.IntegerField()
+
 
 def __str__(self):
-        return self.fname
-    
-
-   
-
-    
+    return self.fname
--- dbms/views.py
+++ dbms/views.py
@@ -1,35 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from dbms.models import booking,approved
+from dbms.models import booking, approved
 
 # Create your views here.
+
+
 def index(request):
-    if request.method=="POST":
+    if request.method == "POST":
         fname = request.POST.get('fname', '')
         lname = request.POST.get('lname', '')
         email = request.POST.get('email', '')
         city = request.POST.get('city', '')
         state = request.POST.get('state', '')
         pin = request.POST.get('pin', '')
-        print(fname,lname,email,city,state,pin)
-        bks = booking(fname=fname,lname=lname,email=email,city=city,state=state,pin=pin)
+        print(fname, lname, email, city, state, pin)
+        bks = booking(fname=fname, lname=lname, email=email,
+                      city=city, state=state, pin=pin)
         bks.save()
         print("hi")
     else:
         print('hello')
-    return render(request,'dbms\index.html')
+    return render(request, 'dbms\index.html')
+
+
 def submit(request):
-    return render(request,'dbms\submitted.html')
+    return render(request, 'dbms\submitted.html')
+
+
 def adminp(request):
-    if 'form_rejected' in request.POST and request.method=="POST":
+    if 'form_rejected' in request.POST and request.method == "POST":
         print("Went into reject")
-        p=booking.objects.filter(id=request.POST.get('object_id','')).delete()
+        p = booking.objects.filter(
+            id=request.POST.get('object_id', '')).delete()
         print(p)
-    elif 'form_approved' in request.POST and request.method=="POST":
+    elif 'form_approved' in request.POST and request.method == "POST":
         print("went in approve")
         booking_obj = booking.objects.get(id=request.POST.get('object_id'))
-        approved_obj=approved.objects.create(fname=booking_obj.fname,lname=booking_obj.lname,email=booking_obj.email,city=booking_obj.city,state=booking_obj.state,pin=booking_obj.pin)
+        approved_obj = approved.objects.create(fname=booking_obj.fname, lname=booking_obj.lname,
+                                               email=booking_obj.email, city=booking_obj.city, state=booking_obj.state, pin=booking_obj.pin)
         booking_obj.delete()
-    x=booking.objects.all()
-    params={'pro': x}
-    return render(request,'dbms/adminpanel.html',params)
+    x = booking.objects.all()
+    params = {'pro': x}
+    return render(request, 'dbms/adminpanel.html', params)
